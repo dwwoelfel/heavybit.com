724958758c1fac8c3a97b285fb2379413fab66d5
James Lindenbaum <james@heroku.com>
2011-08-01 13:46:16 -0700

    x

--- main.rb
+++ main.rb
@@ -1,5 +1,26 @@
+require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "heavybit"
+  "<html>
+		<head>
+			<title>heavybit</title>
+			<style>
+				body {
+					background: black;
+				}
+				div {
+					color: white;
+					margin-top: 250px;
+					font-size: 40pt;
+					text-align: center;
+					font-family: Verdana;
+					font-weight: bold;
+				}
+			</style>
+		</head>
+		<body>
+			<div>heavybit</div>
+		</body>
+	</html>"
 end
